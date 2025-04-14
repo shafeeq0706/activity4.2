7a10624997a4a59be8e05cb30c169f67bd635362
shafeeq0706 <mschoomka@hotmail.com>
2025-04-14 11:31:13 +0100

    Update app.py

--- flask_project/app.py
+++ flask_project/app.py
@@ -36,5 +36,9 @@ def index():
 
     return render_template('index.html', main_options=main_options, sub_option=sub_option)
 
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
